0936708faa5736f0be8c071251da8b309298a0fa
aenosys <aenosys@gmail.com>
2025-04-20 08:33:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-// Import the express module
 const express = require('express');
-
-// Create an instance of an Express application
 const app = express();
-
-// Define the port the server will listen on
 const port = 3000;
+
+// Timeline logging every second
+let counter = 1;
+setInterval(() => {
+  console.log(`[${new Date().toLocaleTimeString()}] Log #${counter++}`);
+}, 1000);
 
 // Define a route for the root URL ("/")
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
-// Start the server and listen on the specified port
+// Start the server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
